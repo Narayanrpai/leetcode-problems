b765daba747f92e18bf77b8a46b56b445882ae34
Narayan Pai <narayanrpai19@gmail.com>
2022-08-08 19:41:11 +0530

    Update 309. Best Time to Buy and Sell Stock with Cooldown.cpp
    Added tabulation and memory optimization code

--- DP/309. Best Time to Buy and Sell Stock with Cooldown.cpp
+++ DP/309. Best Time to Buy and Sell Stock with Cooldown.cpp
@@ -30,3 +30,62 @@ public:
         return solve(prices, 0, 1, 0);
     }
 };
+
+
+-------------------------------------------------------------------------------------------------------------
+Tabulation nx2x2 ( ͡° ͜ʖ ͡°)
+
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int dp[n+1][3][3];
+        memset(dp, 0, sizeof(dp));
+        
+        for(int i=0;i<=1;i++){
+            for(int j=0;j<=1;j++){
+                dp[n][i][j] = 0;
+            }
+        }
+        
+        for(int i=n-1;i>=0;i--){
+            for(int state=0;state<=1;state++){
+                for(int cooldown=0;cooldown<=1;cooldown++){
+                    if(cooldown == 1){
+                        dp[i][state][cooldown] = dp[i+1][state][0];
+                    }
+                    else{
+                        if(state == 1){
+                            dp[i][state][cooldown] = max(-prices[i] + dp[i+1][0][cooldown], dp[i+1][1][cooldown]);
+                        }
+                        else{
+                            dp[i][state][cooldown] = max(prices[i] + dp[i+1][1][1], dp[i+1][0][cooldown]);
+                        }
+                    }
+                }
+            }
+        }
+        return dp[0][1][0];
+    }
+
+-------------------------------------------------------------------------------------------------------------
+Memory optimization - O(3x3) with TC - O(Nx3x3) (⌐■_■)
+
+        for(int i=n-1;i>=0;i--){
+            for(int state=0;state<=1;state++){
+                for(int cooldown=0;cooldown<=1;cooldown++){
+                    if(cooldown == 1){
+                        prev[state][cooldown] = after[state][0];
+                    }
+                    else{
+                        if(state == 1){
+                            prev[state][cooldown] = max(-prices[i] + after[0][cooldown], after[1][cooldown]);
+                        }
+                        else{
+                            prev[state][cooldown] = max(prices[i] + after[1][1], after[0][cooldown]);
+                        }
+                    }
+                }
+            }
+            after = prev;
+        }
+        return after[1][0];
+    } 

b268b46cd2ddee5fd15eb5eb7179ab404836cf22
Narayan Pai <narayanrpai19@gmail.com>
2022-08-09 17:03:59 +0530

    Update 188. Best Time to Buy and Sell Stock IV.cpp
    Added code for tabulation & space optimization

--- DP/188. Best Time to Buy and Sell Stock IV.cpp
+++ DP/188. Best Time to Buy and Sell Stock IV.cpp
@@ -25,3 +25,54 @@ public:
         return solve(prices, k, 1, 0, 0);
     }
 };
+
+-------------------------------------------------------------------------------------------------------------
+Tabulation SC O(n*2*k) TC O(n*2*k) :|
+
+class Solution {
+public:
+    
+    int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
+        int dp[n+1][2*k+1];
+        memset(dp, 0, sizeof(dp));
+        
+        for(int i=n-1;i>=0;i--){
+            for(int j=2*k-1;j>=0;j--){
+                if(j % 2 == 0){
+                    dp[i][j] = max(-prices[i] + dp[i+1][j+1], dp[i+1][j]);
+                }
+                else{
+                    dp[i][j] = max(prices[i] + dp[i+1][j+1], dp[i+1][j]);
+                }
+            }
+            
+        }
+        return dp[0][0];
+    }
+};
+
+-------------------------------------------------------------------------------------------------------------
+Memory Optimized O(2*k + 2*k)
+
+class Solution {
+public:
+    
+    int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
+        vector<int> pre(2*k+2, 0), after(2*k+2, 0);
+        
+        for(int i=n-1;i>=0;i--){
+            for(int j=2*k-1;j>=0;j--){
+                if(j % 2 == 0){
+                    pre[j] = max(-prices[i] + after[j+1], after[j]);
+                }
+                else{
+                    pre[j] = max(prices[i] + after[j+1], after[j]);
+                }
+            }
+            after = pre;
+        }
+        return after[0];
+    }
+};
